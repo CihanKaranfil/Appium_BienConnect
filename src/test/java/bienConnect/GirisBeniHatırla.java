7fde88236abceb1fc308b33bb71c65766251c76c
cihankaranfil44@hotmail.com <cihankaranfil44@hotmail.com>
2024-03-22 10:48:56 +0300

    22.03.2024 "v1"

---
+++ src/test/java/bienConnect/GirisBeniHatırla.java
@@ -0,0 +1,59 @@
+package bienConnect;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+import utilities.ReusableMethods;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class GirisBeniHatırla extends ReusableMethods {
+    @Test
+    public void test01() throws MalformedURLException, InterruptedException {
+        String appUrl = System.getProperty("user.dir")
+                + File.separator + "src"
+                + File.separator + "test"
+                + File.separator + "resources"
+                + File.separator + "BienConnect.apk";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "android");
+        capabilities.setCapability("automationName", "uiautomator2");
+        capabilities.setCapability("udid", "emulator-5554");
+        capabilities.setCapability("app", appUrl);
+
+        URL url = new URL("http://0.0.0.0:4723");
+        AndroidDriver driver = new AndroidDriver(url, capabilities);
+
+        WebElement kullaiciAdi = driver.findElement(AppiumBy.
+                xpath("//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText[1]"));
+        kullaiciAdi.click();
+        kullaiciAdi.sendKeys("Uyumsoft");
+
+        WebElement sifre = driver.findElement(AppiumBy.
+                xpath("//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText[2]"));
+        sifre.click();
+        sifre.sendKeys("Uyumsoft");
+
+        WebElement beniHatirla = driver.findElement(AppiumBy.className("android.widget.CheckBox"));
+        beniHatirla.click();
+
+        WebElement girisYap = driver.findElement(AppiumBy.accessibilityId("Giriş Yap"));
+        girisYap.click();
+        Thread.sleep(3000);
+
+        //WebElement menuButton = driver.findElement(AppiumBy.className("android.widget.Button"));
+        //menuButton.click();
+
+        clickGesture(driver,536,2155);
+       // clickGesture(driver,830,2155);
+
+        clickGesture(driver,350,500);
+
+
+    }
+}
--- src/test/java/bienConnect/GirisModulü.java
+++ src/test/java/bienConnect/GirisModulü.java
@@ -43,7 +43,107 @@ public class GirisModulü {
 
         WebElement girisYap = driver.findElement(AppiumBy.accessibilityId("Giriş Yap"));
         girisYap.click();
+    }
+
+    @Test
+    public void test001() throws MalformedURLException {
+        String appUrl = System.getProperty("user.dir")
+                + File.separator + "src"
+                + File.separator + "test"
+                + File.separator + "resources"
+                + File.separator + "BienConnect.apk";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "android");
+        capabilities.setCapability("automationName", "uiautomator2");
+        capabilities.setCapability("udid", "emulator-5554");
+        capabilities.setCapability("app", appUrl);
+        //capabilities.setCapability("appPackage", "io.appium.android.apis");
+        //capabilities.setCapability("appActivity", "io.appium.android.apis.ApiDemos");
+
+        URL url = new URL("http://0.0.0.0:4723");
+        AndroidDriver driver = new AndroidDriver(url, capabilities);
 
 
+        WebElement kullaiciAdi = driver.findElement(AppiumBy.
+                xpath("//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText[1]"));
+        kullaiciAdi.click();
+        kullaiciAdi.sendKeys("Uyumsoft");
+
+        WebElement sifre = driver.findElement(AppiumBy.
+                xpath("//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText[2]"));
+        sifre.click();
+        sifre.sendKeys("Uyumsoft");
+
+        WebElement girisYap = driver.findElement(AppiumBy.accessibilityId("Giriş Yap"));
+        girisYap.click();
+    }
+
+    @Test
+    public void test02() throws MalformedURLException {
+        String appUrl = System.getProperty("user.dir")
+                + File.separator + "src"
+                + File.separator + "test"
+                + File.separator + "resources"
+                + File.separator + "BienConnect.apk";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "android");
+        capabilities.setCapability("automationName", "uiautomator2");
+        capabilities.setCapability("udid", "emulator-5554");
+        capabilities.setCapability("app", appUrl);
+        //capabilities.setCapability("appPackage", "io.appium.android.apis");
+        //capabilities.setCapability("appActivity", "io.appium.android.apis.ApiDemos");
+
+        URL url = new URL("http://0.0.0.0:4723");
+        AndroidDriver driver = new AndroidDriver(url, capabilities);
+
+
+        WebElement kullaiciAdi = driver.findElement(AppiumBy.
+                xpath("//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText[1]"));
+        kullaiciAdi.click();
+        kullaiciAdi.sendKeys("Uyumsoft1");
+
+        WebElement sifre = driver.findElement(AppiumBy.
+                xpath("//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText[2]"));
+        sifre.click();
+        sifre.sendKeys("Uyumsoft");
+
+        WebElement girisYap = driver.findElement(AppiumBy.accessibilityId("Giriş Yap"));
+        girisYap.click();
+    }
+
+    @Test
+    public void test03() throws MalformedURLException {
+        String appUrl = System.getProperty("user.dir")
+                + File.separator + "src"
+                + File.separator + "test"
+                + File.separator + "resources"
+                + File.separator + "BienConnect.apk";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "android");
+        capabilities.setCapability("automationName", "uiautomator2");
+        capabilities.setCapability("udid", "emulator-5554");
+        capabilities.setCapability("app", appUrl);
+        //capabilities.setCapability("appPackage", "io.appium.android.apis");
+        //capabilities.setCapability("appActivity", "io.appium.android.apis.ApiDemos");
+
+        URL url = new URL("http://0.0.0.0:4723");
+        AndroidDriver driver = new AndroidDriver(url, capabilities);
+
+
+        WebElement kullaiciAdi = driver.findElement(AppiumBy.
+                xpath("//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText[1]"));
+        kullaiciAdi.click();
+        kullaiciAdi.sendKeys("Uyumsoft");
+
+        WebElement sifre = driver.findElement(AppiumBy.
+                xpath("//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText[2]"));
+        sifre.click();
+        sifre.sendKeys("Uyumsoft1");
+
+        WebElement girisYap = driver.findElement(AppiumBy.accessibilityId("Giriş Yap"));
+        girisYap.click();
     }
 }
